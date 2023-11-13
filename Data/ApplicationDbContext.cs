2a671fc623ad6ad0708241c0beb3b7b0600e46c4
gustavotorrezan1 <gustorrezani@gmail.com>
2023-11-13 17:18:45 -0300

    Criado Api

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using MeuTodoApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeuTodoApi.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Todo> Todos { get; set; }
+}
---
+++ Models/Todo.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MeuTodoApi.Models;
+
+public class Todo
+{
+    public int Id { get; set; }
+
+    [DisplayName("Título")]
+    [Required(ErrorMessage = "Campo obrigatório")]
+    public string Title { get; set; }
+
+    [DisplayName("Concluído")]
+    public bool Done { get; set; }
+
+    [DisplayName("Criado em")]
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+    [DisplayName("Última atualização")]
+    public DateTime LastUpdateData { get; set; } = DateTime.Now;
+
+    [DisplayName("Usuário")]
+    public string User { get; set; }
+}
